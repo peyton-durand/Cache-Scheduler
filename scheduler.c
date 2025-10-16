858342c152766b588573481a89f190b5f8373bb0
peyton-durand <pdurand365@outlook.com>
2025-10-16 16:51:03 -0700

    Finished parser

--- scheduler.c
+++ scheduler.c
@@ -5,31 +5,42 @@
 
 // Headers as needed
 
-const uint32_t SEED_VALUE = 200;  // Seed value for reading from file
+const int SEED_VALUE = 200;  // Seed value for reading from file
 
 typedef enum {false, true} bool;        // Allows boolean types in C
 
 /* Defines a job struct */
 typedef struct Process {
-    uint32_t A;                         // A: Arrival time of the process
-    uint32_t B;                         // B: Upper Bound of CPU burst times of the given random integer list
-    uint32_t C;                         // C: Total CPU time required
-    uint32_t M;                         // M: Multiplier of CPU burst time
-    uint32_t processID;                 // The process ID given upon input read
+    int A;                         // A: Arrival time of the process
+    int B;                         // B: Upper Bound of CPU burst times of the given random integer list
+    int C;                         // C: Total CPU time required
+    int M;                         // M: Multiplier of CPU burst time
+    int processID;                 // The process ID given upon input read
+    int currentCPUTimeRun;        // The current amount of CPU time the process has run for
+    int currentIOBlockedTime;     // The current amount of time the process has
+    int currentWaitingTime;       // The current amount of time the process has spent waiting
+    int finishingTime;            // The time the process finished
+    int currentCPUBurst;          // The current CPU burst time remaining
+    int currentIOBurst;           // The current IO burst time remaining
 
     // Add other fields as needed
 } _process;
 
-uint32_t TOTAL_CREATED_PROCESSES = 0;   // The total number of processes constructed
+int TOTAL_CREATED_PROCESSES = 0;   // The total number of processes constructed
+
+int TOTAL_FINISHED_PROCESSES = 0;  // The total number of processes that have finished
+
+int CURRENT_CYCLE = 0;             // The current cycle of the scheduler
+
+int TOTAL_NUMBER_OF_CYCLES_SPENT_BLOCKED = 0; // The total number of cycles spent in the blocked state
 
 // Additional variables as needed
 
 
-/**
- * Reads a random non-negative integer X from a file with a given line named random-numbers (in the current directory)
- */
-uint32_t getRandNumFromFile(uint32_t line, FILE* random_num_file_ptr){
-    uint32_t end, loop;
+
+// reads a random non-negative integer X from a file with a given line named random-numbers (in the current directory)
+int getRandNumFromFile(int line, FILE* random_num_file_ptr){
+    int end, loop;
     char str[512];
 
     rewind(random_num_file_ptr); // reset to be beginning
@@ -40,11 +51,11 @@ uint32_t getRandNumFromFile(uint32_t line, FILE* random_num_file_ptr){
         }
     }
     if(!end) {
-        return (uint32_t) atoi(str);
+        return (int) atoi(str);
     }
 
-    // Fail-safe return
-    return (uint32_t) 1804289383;
+    // fail-safe return
+    return (int) 1804289383;
 }
 
 
@@ -53,12 +64,12 @@ uint32_t getRandNumFromFile(uint32_t line, FILE* random_num_file_ptr){
  * Reads a random non-negative integer X from a file named random-numbers.
  * Returns the CPU Burst: : 1 + (random-number-from-file % upper_bound)
  */
-uint32_t randomOS(uint32_t upper_bound, uint32_t process_indx, FILE* random_num_file_ptr)
+int randomOS(int upper_bound, int process_indx, FILE* random_num_file_ptr)
 {
     char str[20];
     
-    uint32_t unsigned_rand_int = (uint32_t) getRandNumFromFile(SEED_VALUE+process_indx, random_num_file_ptr);
-    uint32_t returnValue = 1 + (unsigned_rand_int % upper_bound);
+    int unsigned_rand_int = (int) getRandNumFromFile(SEED_VALUE+process_indx, random_num_file_ptr);
+    int returnValue = 1 + (unsigned_rand_int % upper_bound);
 
     return returnValue;
 } 
@@ -67,22 +78,8 @@ uint32_t randomOS(uint32_t upper_bound, uint32_t process_indx, FILE* random_num_
 /********************* SOME PRINTING HELPERS *********************/
 
 
-/**
- * Prints to standard output the original input
- * process_list is the original processes inputted (in array form)
- */
-void printStart(_process process_list[])
-{
-    printf("The original input was: %i", TOTAL_CREATED_PROCESSES);
 
-    uint32_t i = 0;
-    for (; i < TOTAL_CREATED_PROCESSES; ++i)
-    {
-        printf(" ( %i %i %i %i)", process_list[i].A, process_list[i].B,
-               process_list[i].C, process_list[i].M);
-    }
-    printf("\n");
-} 
+void printInput(_process process_list[]);
 
 /**
  * Prints to standard output the final output
@@ -92,7 +89,7 @@ void printFinal(_process finished_process_list[])
 {
     printf("The (sorted) input is: %i", TOTAL_CREATED_PROCESSES);
 
-    uint32_t i = 0;
+    int i = 0;
     for (; i < TOTAL_FINISHED_PROCESSES; ++i)
     {
         printf(" ( %i %i %i %i)", finished_process_list[i].A, finished_process_list[i].B,
@@ -107,7 +104,7 @@ void printFinal(_process finished_process_list[])
  */
 void printProcessSpecifics(_process process_list[])
 {
-    uint32_t i = 0;
+    int i = 0;
     printf("\n");
     for (; i < TOTAL_CREATED_PROCESSES; ++i)
     {
@@ -128,12 +125,12 @@ void printProcessSpecifics(_process process_list[])
  */
 void printSummaryData(_process process_list[])
 {
-    uint32_t i = 0;
+    int i = 0;
     double total_amount_of_time_utilizing_cpu = 0.0;
     double total_amount_of_time_io_blocked = 0.0;
     double total_amount_of_time_spent_waiting = 0.0;
     double total_turnaround_time = 0.0;
-    uint32_t final_finishing_time = CURRENT_CYCLE - 1;
+    int final_finishing_time = CURRENT_CYCLE - 1;
     for (; i < TOTAL_CREATED_PROCESSES; ++i)
     {
         total_amount_of_time_utilizing_cpu += process_list[i].currentCPUTimeRun;
@@ -172,32 +169,58 @@ void printSummaryData(_process process_list[])
 // argv[0] is the name of the program, argv[1] is the first command line argument, argv[2] is the second, etc.
 int main(int argc, char *argv[])
 {
-    _process process_list[TOTAL_CREATED_PROCESSES]; // creates a container for all processes
 
-
-
-
-    // parse command line arguments
+    // ensure proper command line arguments
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <input_file>\n", argv[0]); // fprintf allows us to print to stderr instead of stdout
         return 1;
     }
+
+    // open the input file
     char *input_file = argv[1];
     FILE *file_ptr = fopen(input_file, "r");
     if (file_ptr == NULL) {
         fprintf(stderr, "Error: Could not open file %s\n", input_file);
         return 1;
     }
-    fscanf(file_ptr, "%u", &TOTAL_CREATED_PROCESSES); // read the number of processes from the file
+
+    fscanf(file_ptr, "%d", &TOTAL_CREATED_PROCESSES); // read the number of processes from the file
+    _process process_list[TOTAL_CREATED_PROCESSES]; // array to hold the processes
+
+    // read the processes from the file
     for (int i = 0; i < TOTAL_CREATED_PROCESSES; i++) {
-        fscanf(file_ptr, "%u %u %u %u", &process_list[i].A, &process_list[i].B, &process_list[i].C, &process_list[i].M);
+        fscanf(file_ptr, " (%d %d %d %d)", &process_list[i].A, &process_list[i].B, &process_list[i].C, &process_list[i].M);
         process_list[i].processID = i;
     }
+
     fclose(file_ptr);
+    printInput(process_list);
+    
 
 
 
-    // Write code for your shiny scheduler
 
+
+
+
+
+
+    printf("Success!\n");
     return 0;
-} 
+}
+
+// FUNCTION DEFINITIONS ---------------------------------------------------------------------------------
+
+// prints to standard output the original input
+void printInput(_process process_list[])
+{
+    printf("\nInput: %i", TOTAL_CREATED_PROCESSES);
+
+    int i = 0;
+    for (; i < TOTAL_CREATED_PROCESSES; ++i)
+    {
+        printf(" (%d %d %d %d)", process_list[i].A, process_list[i].B,
+               process_list[i].C, process_list[i].M);
+    }
+    printf("\n");
+}
